6dacda8f869956dee2bd2be4c2c1bd6d97542918
abdelrhmanElkady <48697496+abdelrhmanElkady@users.noreply.github.com>
2022-02-15 17:44:48 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(_loginOrigin, builder =>
     {
-        builder.WithOrigins("https://quizzical-northcutt-440537.netlify.app");
+        builder.WithOrigins("https://autobus-complete.netlify.app");
         builder.AllowAnyHeader();
         builder.AllowAnyMethod();
         builder.AllowCredentials();
