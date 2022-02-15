58c73f940c7756b811ad112abe0f618781841bdd
abdelrhmanElkady <48697496+abdelrhmanElkady@users.noreply.github.com>
2022-02-15 17:38:48 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(_loginOrigin, builder =>
     {
-        builder.WithOrigins("https://abdelrhmanelkady.github.io");
+        builder.WithOrigins("https://app.netlify.com");
         builder.AllowAnyHeader();
         builder.AllowAnyMethod();
         builder.AllowCredentials();
